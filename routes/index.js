017e89fc5a0193f22fbee90bc3fcdda6caf3e999
asyifa-mg <asyifamaharanigustina12@gmail.com>
2024-04-19 08:52:49 +0700

    Add companyRouter.js for company route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const authRouter = require("./authRouter");
+const companyRouter = require("./companyRouter");
 
 router.use("/api/v1", authRouter);
+router.use("/api/v1/company", companyRouter);
 
 module.exports = router;
