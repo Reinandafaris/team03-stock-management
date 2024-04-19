cac33a74d63d8177ec0446d88694046885b4978e
Reinandafaris <reinandafaris7@gmail.com>
2024-04-19 11:51:39 +0700

    Add createItem route and controller for creating new items

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,6 @@ const itemRouter = require("./itemRouter");
 
 router.use("/api/v1", authRouter);
 router.use("/api/v1/company", companyRouter);
-router.use("/api/v1/item", itemRouter);
+router.use("/api/v1/items", itemRouter);
 
 module.exports = router;
--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const itemController = require("../controllers/itemControllers");
 
 router.get("/", itemController.getItems);
+router.post("/create", itemController.createItem);
 
 module.exports = router;
