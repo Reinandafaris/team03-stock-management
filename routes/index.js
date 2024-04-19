c557e78aa2a68c561e7d59763326f7ce1d5c0847
QIAF <qonitaafifah3@gmail.com>
2024-04-19 16:15:42 +0700

    Add categoryRouter.js for handle category route

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const authRouter = require('./authRouter');
 const companyRouter = require('./companyRouter');
 const stockRouter = require('./stockRouter');
 const itemRouter = require('./itemRouter');
+const categoryRouter = require('./categoryRouter');
 
 router.use('/api/v1', authRouter);
 router.use('/api/v1/company', companyRouter);
 router.use('/api/v1/stock', stockRouter);
 router.use('/api/v1/items', itemRouter);
+router.use('/api/v1/category', categoryRouter);
 
 module.exports = router;
