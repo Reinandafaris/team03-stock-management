b04f4b9501cf3a325c8ac4930d3167d728e35f1d
ItsMeAldy <rizaldimustakim10@gmail.com>
2024-04-19 14:29:49 +0800

    Add migration and generate model for stock

---
+++ models/stock.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Stock extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Stock.init({
+    companyId: DataTypes.STRING,
+    itemId: DataTypes.STRING,
+    stock: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Stock',
+  },{
+    freezeTableName: true
+    });
+  return Stock;
+};
