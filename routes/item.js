fa5990bd97d0abaae5003c3c1e5f08d3f3ba1b71
Reinandafaris <reinandafaris7@gmail.com>
2024-04-21 11:47:23 +0700

    Refactor routes and controllers for item management

--- routes/item.js
+++ routes/item.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const { getAllItem, createItem } = require('../controllers/item');
+const express = require("express");
+const {
+  getAllItem,
+  createItem,
+  updateItem,
+  deleteItem,
+} = require("../controllers/item");
 
 const router = express.Router();
 
-router.get('/', getAllItem);
-router.post('/create', createItem);
+router.get("/", getAllItem);
+router.post("/create", createItem);
+router.patch("/update/:id", updateItem);
+router.delete("/delete/:id", deleteItem);
 
 module.exports = router;
